bb596bcfc98dc9772ffad6002fa15b5891a0d51c
Lindsey Bowen <lindsbow102@yahoo.com>
2018-04-24 15:37:55 -0700

    getting there

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,27 @@
-var express = require("express");
+var path = require('path');
+
+//Export HTML Routes
+module.exports = function(app) {
+
+    //Home page
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname, '../public/home.html'));
+    });
+
+    //Survey page
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    });
+};
+
+
+/*var express = require("express");
 
 var app = express();
 var PORT = 8080;
+
+app.get("../../public/survey.html", function(req, res) {
+    res.send("Welcome to the Star Wars Page!");
+  });*/
+  
 
--- app/server.js
+++ app/server.js
@@ -3,3 +3,20 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+var port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+  });
