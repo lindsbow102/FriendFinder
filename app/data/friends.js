99daf4334b1a3eb57906137f816da4027ba83fdd
Lindsey Bowen <lindsbow102@yahoo.com>
2018-04-23 19:03:03 -0700

    init commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Pink',
+		photo: 'http://image.boomsbeat.com/data/images/full/13767/pink.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sheldon Cooper',
+		photo: 'https://ia.media-imdb.com/images/M/MV5BZjg4MGNlZDgtMmM5OC00ZmMxLTg3Y2EtZmZjOGJlNDU4NGNhXkEyXkFqcGdeQXVyMTgxOTIzNzk@._V1_UY666_CR259,0,666,666_AL_.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Homer Simpson',
+		photo: 'https://images-na.ssl-images-amazon.com/images/I/41b2Y9DaQ3L._SX342_.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Sophia Petrillo',
+		photo: 'https://cdn.skim.gs/image/upload/b_rgb:FFFFFF,bo_1px_solid_rgb:EDEDED,c_pad,f_auto,fl_lossy,h_382,q_auto,w_682/5ce13d258378e68faa83',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Barney Stinson',
+		photo: 'http://images6.fanpop.com/image/photos/35000000/-neil-patrick-harris-barney-stinson-35089250-465-500.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Oscar the Grouch',
+		photo: 'https://pbs.twimg.com/profile_images/2678877977/617fd5d2ee2151b992e5617d9c834e8b_400x400.jpeg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Liz Lemon',
+		photo: 'http://3.bp.blogspot.com/_b3G_kBQdhm4/S8kf3gU5jXI/AAAAAAAAAYs/otsZGOUH1_I/s1600/0000041318_20070711111319.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Saul Goodman',
+		photo: 'https://vignette.wikia.nocookie.net/villains/images/4/47/SaulGoodman.jpg/revision/latest?cb=20120916191112',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Rogelio',
+		photo: 'https://pbs.twimg.com/profile_images/663964270674116608/ITwD8gHt_400x400.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Phoebe Buffay',
+		photo: 'http://images4.fanpop.com/image/polls/550000/550939_1285891901356_full.jpg?v=1285892204',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
