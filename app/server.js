07416bef60ddc2c8ca0dc564f0988079f9e2ad34
Lindsey Bowen <lindsbow102@yahoo.com>
2018-04-24 18:12:59 -0700

    added port

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, './app/public')));
 
